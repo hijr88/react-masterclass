2270cc61669e0f2d17249aaac6aac8a9678fdabe
yohan <hijr88@naver.com>
2022-02-28 10:54:25 +0900

    Router Link를 통해 state 넘기기 코인 이미지 가져오기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,50 @@
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface RouteState {
+  state:{
+    name:string;
+  };
+}
 
 function Coin () {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
-  console.log(typeof coinId);
-  return <h1>Coin: {coinId}</h1>;
+  const { state } = useLocation() as RouteState;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || 'Loading'}</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : null}
+    </Container>
+  )
 }
 
 export default Coin;
