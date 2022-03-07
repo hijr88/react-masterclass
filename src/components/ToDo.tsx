ca521902080c3be88bcf046238fe74f545c8ffa1
yohan <hijr88@naver.com>
2022-03-07 13:53:21 +0900

    카테고리 수정기능 추가(불변성 상태변경)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { IToDo } from '../atoms';
+import { IToDo, toDoState } from '../atoms';
+import { useSetRecoilState } from 'recoil';
 
 function ToDo({ text, id, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name }
     } = event;
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo['category'] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1)
+      ];
+    });
   };
   return (
     <li>
