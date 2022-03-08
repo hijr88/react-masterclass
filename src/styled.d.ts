83ef7aac521ce0987e5237ce17fd32cd24d5926f
yohan <hijr88@naver.com>
2022-03-08 15:03:46 +0900

    Styles and Placeholders(드래그 하는동안 영역 변경 방지)

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,9 +2,8 @@ import 'styled-components';
 //테마 정의 확장
 declare module 'styled-components' {
   export interface DefaultTheme {
-    textColor: string;
     bgColor: string;
-    accentColor: string;
-    cardBgColor: string;
+    boardColor: string;
+    cardColor: string;
   }
 }
