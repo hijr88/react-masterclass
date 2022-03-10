3fd07a469dc34e82edce7cac59c25dd13cc81ca5
yohan <hijr88@naver.com>
2022-03-10 18:17:14 +0900

    framer-motion 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { motion} from 'framer-motion';
 
 const Wrapper = styled.div`
   display: flex;
@@ -20,6 +21,7 @@ function App() {
   return (
     <Wrapper>
       <Box/>
+      <motion.div>hello</motion.div>
     </Wrapper>
   );
 }
