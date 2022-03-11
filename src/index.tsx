85497345f0264274b10d4811557bd1c40067d638
yohan <hijr88@naver.com>
2022-03-11 13:21:41 +0900

    마우스 이벤트(hover, click)

--- src/index.tsx
+++ src/index.tsx
@@ -27,46 +27,55 @@ const GlobalStyle = createGlobalStyle`
     font: inherit;
     vertical-align: baseline;
   }
+
   /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure,
   footer, header, hgroup, main, menu, nav, section {
     display: block;
   }
+
   /* HTML5 hidden-attribute fix for newer browsers */
   *[hidden] {
     display: none;
   }
+
   body {
     line-height: 1;
   }
+
   menu, ol, ul {
     list-style: none;
   }
+
   blockquote, q {
     quotes: none;
   }
+
   blockquote:before, blockquote:after,
   q:before, q:after {
     content: '';
     content: none;
   }
+
   table {
     border-collapse: collapse;
     border-spacing: 0;
   }
-  
+
   //--------새로운값
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
   * {
     box-sizing: border-box;
   }
+
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
     color: black;
     line-height: 1.2;
-    background:linear-gradient(135deg,#e09,#d0e);
+    background: linear-gradient(135deg, #f8bef9, #a280e5);
   }
+
   a {
     text-decoration: none;
     color: inherit;
