fd71891dbcb4726f992b302669e2ef88ab66760b
yohan <hijr88@naver.com>
2022-03-18 14:04:47 +0900

    영화 상세보기 -2 Overlay 및 Modal 스크롤위치 중앙에 나타나게하기

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence, motion, useViewportScroll } from 'framer-motion';
 import { getMovies, IGetMoviesResult } from '../api';
 import { makeImagePath } from '../utils';
 import { useState } from 'react';
@@ -110,11 +110,30 @@ const boxInfoVariants = {
   }
 };
 
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`;
+
+const BigMovie = styled(motion.div)`
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+`;
+
 const offset = 6;
 
 function Home() {
   const navigate = useNavigate();
   const bigMovieMatch = useMatch('/movies/:movieId');
+  const { scrollY } = useViewportScroll();
   const { data, isLoading } = useQuery<IGetMoviesResult>(['movies', 'nowPlaying'], getMovies);
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
@@ -131,6 +150,7 @@ function Home() {
   const onBoxClicked = (movieId: number) => {
     navigate(`/movies/${movieId}`);
   };
+  const onOverlayClick = () => navigate('');
   return (
     <Wrapper>
       {isLoading ? <Loader>Loading...</Loader>
@@ -169,10 +189,13 @@ function Home() {
           </Slider>
           <AnimatePresence>
             {bigMovieMatch &&
-                <motion.div
-                    style={{ position: 'absolute', width: '40vw', height: '80vh', backgroundColor: 'red', top: 50, left: 0, right: 0, margin: '0 auto'}}
-                    layoutId={bigMovieMatch.params.movieId}
+              <>
+                <Overlay onClick={onOverlayClick} animate={{ opacity: 1 }} exit={{ opacity: 0 }}/>
+                <BigMovie
+                  style={{ top: scrollY.get() + 100 }}
+                  layoutId={bigMovieMatch.params.movieId}
                 />
+              </>
             }
           </AnimatePresence>
         </>}
